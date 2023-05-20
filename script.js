64f9e67151d39df1718809f82ca856bb161645ff
Hellper <40011027+Heckper@users.noreply.github.com>
2023-05-20 19:02:03 +0100

    scroll bar

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
+let scrollBar = document.querySelector('.scrollbar');
+let thumb = document.querySelector('.thumb');
+
+
+const body = document.body;
+const html = document.documentElement;
+const height = Math.max(body.getBoundingClientRect().height, html.getBoundingClientRect().height);
+
+
 
 
 window.onscroll = () => {
+    let topS = html.scrollTop;
+    let percent = (topS * 100) / height;
+    console.log(percent);
+    
+    thumb.style.transform = "translateY(" + percent + "vh)";
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
@@ -17,3 +31,7 @@ window.onscroll = () => {
         };
     });
 };
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
